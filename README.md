6f5d985ffd24bb3fec081545a9fb087fcbd71143
coderaiser <coderaiser@cloudcmd.io>
2016-12-21 17:29:40 +0200

    feature(nomine) add

---
+++ README.md
@@ -0,0 +1,73 @@
+# Nomine [![License][LicenseIMGURL]][LicenseURL] [![NPM version][NPMIMGURL]][NPMURL] [![Dependency Status][DependencyStatusIMGURL]][DependencyStatusURL] [![Build Status][BuildStatusIMGURL]][BuildStatusURL] [![Coverage Status][CoverageIMGURL]][CoverageURL]
+
+File renamer as express middleware.
+
+## Install
+
+`npm i nomine --save`
+
+## API
+
+```js
+const express = require('express');
+const app = express();
+
+app.use(nomine({
+    prefix: 'rename' // default
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
+```
+
+## Usage Example
+
+After starting web-server with used `nomine` middleware it waits for payload sended on `/rename` url (by default).
+Payload could look this way:
+
+```json
+{
+    "dir": "/",
+    "from": [
+        "bin"
+    ],
+    "to": [
+        "bin2"
+    ]
+}
+```
+
+```sh
+$ curl -X PUT -d '{"dir":"/","from":["bin"], "to": ["bin2"]}' http://localhost:3000/rename
+EACCES: permission denied, rename '/bin' -> '/bin2'
+```
+
+## Environments
+
+In old `node.js` environments that not fully supports `es2015`, `nomine` could be used with:
+
+```js
+var nomine = require('nomine/legacy');
+```
+
+## Related
+
+- [Renamify](https://github.com/coderaiser/node-renamify "Renamify") - rename group of files from a directory
+
+## License
+
+MIT
+
+[NPMIMGURL]:                https://img.shields.io/npm/v/nomine.svg?style=flat
+[BuildStatusIMGURL]:        https://img.shields.io/travis/coderaiser/node-nomine/master.svg?style=flat
+[DependencyStatusIMGURL]:   https://img.shields.io/gemnasium/coderaiser/node-nomine.svg?style=flat
+[LicenseIMGURL]:            https://img.shields.io/badge/license-MIT-317BF9.svg?style=flat
+[NPMURL]:                   https://npmjs.org/package/nomine "npm"
+[BuildStatusURL]:           https://travis-ci.org/coderaiser/node-nomine  "Build Status"
+[DependencyStatusURL]:      https://gemnasium.com/coderaiser/node-nomine "Dependency Status"
+[LicenseURL]:               https://tldrlegal.com/license/mit-license "MIT License"
+
+[CoverageURL]:              https://coveralls.io/github/coderaiser/node-nomine?branch=master
+[CoverageIMGURL]:           https://coveralls.io/repos/coderaiser/node-nomine/badge.svg?branch=master&service=github
+
---
+++ test/nomine.js
@@ -0,0 +1,192 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+
+const test = require('tape');
+const fetch = require('node-fetch');
+
+const before = require('./before');
+const {stringify} = JSON;
+
+test('not found', (t) => {
+    before((port, done) => {
+        fetch(`http://localhost:${port}`)
+            .then((res) => {
+                t.equal(res.status, 404, 'should not found');
+                done();
+                t.end();
+            });
+    });
+});
+
+test('wrong method', (t) => {
+    before((port, done) => {
+        fetch(`http://localhost:${port}/rename`)
+            .then((res) => res.text())
+            .then((text) => {
+                t.equal(text, 'method should be PUT', 'should send message');
+                done();
+                t.end();
+            });
+    });
+});
+
+test('put args: no dir', (t) => {
+    before((port, done) => {
+        const options = {
+            method: 'PUT',
+            body: stringify({})
+        };
+        
+        fetch(`http://localhost:${port}/rename`, options)
+            .then((res) => res.text())
+            .then((text) => {
+                t.equal(text, 'dir should be a string!', 'should send message about dir');
+                done();
+                t.end();
+            });
+    });
+});
+
+test('put args: no from', (t) => {
+    before((port, done) => {
+        const options = {
+            method: 'PUT',
+            body: stringify({
+                dir: '/hello'
+            })
+        };
+        
+        fetch(`http://localhost:${port}/rename`, options)
+            .then((res) => res.text())
+            .then((text) => {
+                t.equal(text, 'from should be an array!', 'should send message about names');
+                done();
+                t.end();
+            });
+    });
+});
+
+test('put args: no from', (t) => {
+    before((port, done) => {
+        const options = {
+            method: 'PUT',
+            body: stringify({
+                dir: '/hello',
+                from: []
+            })
+        };
+        
+        fetch(`http://localhost:${port}/rename`, options)
+            .then((res) => res.text())
+            .then((text) => {
+                t.equal(text, 'to should be an array!', 'should send message about names');
+                done();
+                t.end();
+            });
+    });
+});
+
+test('rename: empty', (t) => {
+    before((port, done) => {
+        const options = {
+            method: 'PUT',
+            body: stringify({
+                dir: '/hello',
+                from: [],
+                to: []
+            })
+        };
+        
+        fetch(`http://localhost:${port}/rename`, options)
+            .then((res) => res.text())
+            .then((text) => {
+                t.equal(text, 'rename: ok', 'should send message about names');
+                done();
+                t.end();
+            });
+    });
+});
+
+test('rename: error', (t) => {
+    before((port, done) => {
+        const options = {
+            method: 'PUT',
+            body: stringify({
+                dir: '/',
+                from: ['bin'],
+                to: ['hello']
+            })
+        };
+        
+        const expected = 'EACCES: permission denied, rename \'/bin\' -> \'/hello\'';
+        
+        fetch(`http://localhost:${port}/rename`, options)
+            .then((res) => res.text())
+            .then((text) => {
+                t.equal(expected, text, 'should send error');
+                done();
+                t.end();
+            });
+    });
+});
+
+test('rename', (t) => {
+    before((port, done) => {
+        const dir = path.join(__dirname, 'fixture', 'two');
+        const from = ['1.txt', '2.txt'];
+        const to = ['a.txt', 'b.txt'];
+        const options = {
+            method: 'PUT',
+            body: stringify({dir, from, to})
+        };
+        
+        const addDir = (name) => {
+            return dir + path.sep + name;
+        };
+        
+        const expected = 'rename: ok';
+        
+        fetch(`http://localhost:${port}/rename`, options)
+            .then((res) => res.text())
+            .then((text) => {
+                t.equal(text, expected, 'should send success message');
+                
+                const oldNames = from.map(addDir);
+                
+                to
+                    .map(addDir)
+                    .forEach((newName, i) => {
+                        fs.renameSync(newName, oldNames[i]);
+                    });
+                
+                done();
+                t.end();
+            });
+    });
+});
+
+test('rename: options: prefix', (t) => {
+    const prefix = '31337';
+    
+    before({prefix}, (port, done) => {
+        const options = {
+            method: 'PUT',
+            body: stringify({
+                dir: '/hello',
+                from: [],
+                to: []
+            })
+        };
+        
+        fetch(`http://localhost:${port}/${prefix}`, options)
+            .then((res) => res.text())
+            .then((text) => {
+                t.equal(text, 'rename: ok', 'should send message about names');
+                done();
+                t.end();
+            });
+    });
+});
+
